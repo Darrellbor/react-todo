a67b979c5eac7e2431fe9b9ff0d469c1a3ab8b2d
Darrell Idiagbor <idiagbordarrel@gmail.com>
2019-01-30 11:17:13 +0100

    chore: deployment files

---
+++ build/precache-manifest.930b1a3d2ff7af6e59378182a0ceeb11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61cc33014507c67be059",
+    "url": "/static/js/main.61cc3301.chunk.js"
+  },
+  {
+    "revision": "1caf7e85b2b8c2376dc1",
+    "url": "/static/js/1.1caf7e85.chunk.js"
+  },
+  {
+    "revision": "61cc33014507c67be059",
+    "url": "/static/css/main.3514f77e.chunk.css"
+  },
+  {
+    "revision": "4727e8e1444fa9cbe04de27681c800b2",
+    "url": "/index.html"
+  }
+];
